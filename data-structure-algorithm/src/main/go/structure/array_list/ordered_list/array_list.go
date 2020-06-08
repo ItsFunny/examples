db974c5cbc96901e52531b8a06dfe641b5de9802
lvcong <lvcong@ffpms.com>
2020-06-08 10:29:02 +0800

    数据结构-线性表-顺序表

---
+++ data-structure-algorithm/src/main/go/structure/array_list/ordered_list/array_list.go
@@ -0,0 +1,17 @@
+/*
+# -*- coding: utf-8 -*-
+# @Author : joker
+# @Time : 2020-06-08 10:21 
+# @File : array_lisr.go
+# @Description : 
+# @Attention : 
+*/
+package ordered_list
+
+type arrayList struct {
+	data []interface{}
+	// 长度
+	size int
+	//
+}
+
