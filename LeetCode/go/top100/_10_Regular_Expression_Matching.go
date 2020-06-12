399c5636e56a99624c13c414edde9e57db7fce90
lvcong <lvcong@ffpms.com>
2020-06-12 10:48:40 +0800

    3sum 求和0

--- LeetCode/go/top100/_10_Regular_Expression_Matching.go
+++ LeetCode/go/top100/_10_Regular_Expression_Matching.go
@@ -3,7 +3,34 @@
 # @Author : joker
 # @Time : 2020-06-09 10:10 
 # @File : _10_Regular_Expression_Matching.go
-# @Description : 
+# @Description :   实现正则匹配
 # @Attention : 
 */
 package main
+
+
+func isMatch(s string, p string) bool {
+	n := len(s)
+	m := len(p)
+	dp := make([][]bool, n + 1)
+	for i := range(dp) {
+		dp[i] = make([]bool, m + 1)
+	}
+	dp[0][0] = true
+	if m > 0 && n > 0 {
+		dp[1][1] = s[0] == p[0] || p[0] == '.'
+	}
+	for j:=2 ;j<=m; j++ {
+		dp[0][j] = dp[0][j-2] && p[j-1] == '*'
+	}
+	for i:=1; i<=n; i+=1 {
+		for j:=2; j<=m; j+=1 {
+			if p[j-1] != '*' {
+				dp[i][j] = dp[i-1][j-1] && (s[i-1] == p[j-1] || p[j-1] == '.')
+			} else {
+				dp[i][j] = dp[i][j-2] || (dp[i-1][j] && (s[i-1]==p[j-2] || p[j-2] == '.'))
+			}
+		}
+	}
+	return dp[n][m]
+}
--- LeetCode/go/top100/_11_Container_With_Most_Water.go
+++ LeetCode/go/top100/_11_Container_With_Most_Water.go
@@ -3,7 +3,35 @@
 # @Author : joker
 # @Time : 2020-06-11 10:53 
 # @File : _11_Container_With_Most_Water.go
-# @Description : 
+# @Description :   最多装水量
 # @Attention : 
 */
 package main
+
+func maxArea(height []int) int {
+	i, j := 0, len(height)-1
+	res := 0
+	for i < j {
+		res = max(res, (j-i)*min(height[i], height[j]))
+		if height[i] < height[j] {
+			i++
+		} else {
+			j--
+		}
+	}
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
