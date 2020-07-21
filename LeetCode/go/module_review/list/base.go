37ab4db014d947943b886ef4cb604a7416166d34
lvcong <lvcong@ffpms.com>
2020-07-21 10:22:19 +0800

    添加链表题

---
+++ LeetCode/go/module_review/list/base.go
@@ -0,0 +1,14 @@
+/*
+# -*- coding: utf-8 -*-
+# @Author : joker
+# @Time : 2020-07-21 09:06 
+# @File : base.go
+# @Description : 
+# @Attention : 
+*/
+package list
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
---
+++ blockchain/ca/main.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+if [[ ! -d "artifacts" ]]; then
+  mkdir artifacts
+else
+   rm -rf artifacts/*
+fi
+
+if [[ ! -d "crypto-config" ]]; then
+    mkdir crypto-config
+else
+    rm -rf crypto-config/*
+fi
+
+export FABRIC_CFG_PATH=${PWD}
+
+cryptogen generate --config=crypto-config.yaml
+
+if [[ $? -ne 0 ]]; then
+    echo "生成证书失败"
+    exit -1
+fi
+
+echo "初始化创世块"
+configtxgen  --profile SoloOrdererGenesis -channelID sysdemochannel -outputBlock ./artifacts/orderer.genesis.block
+echo "生成channel的配置信息"
+configtxgen  --profile DemoChannel  -outputCreateChannelTx ./artifacts/userchannel.tx -channelID demochannel
+echo "生成组织1的锚节点信息"
+configtxgen  --profile DemoChannel -outputAnchorPeersUpdate ./artifacts/demomspanchors.tx -channelID demochannel -asOrg DemoOrgMSP
