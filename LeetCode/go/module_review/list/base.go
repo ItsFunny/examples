52d61d1e553f1d56dd62c825c7b905a52a746fe4
lvcong <lvcong@ffpms.com>
2020-09-10 09:03:36 +0800

    list 61 链表反转 & 109 有序链表转换成伪平衡树

--- LeetCode/go/module_review/list/base.go
+++ LeetCode/go/module_review/list/base.go
@@ -12,3 +12,17 @@ type ListNode struct {
 	Val  int
 	Next *ListNode
 }
+
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
